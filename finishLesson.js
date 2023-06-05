c9c53bb78a13dca09097fbba7abaa324eb6f8f6f
LeeLiron <lee.hocky@gmail.com>
2023-06-05 17:17:34 +0300

    finished?

--- finishLesson.js
+++ finishLesson.js
@@ -1,3 +1,18 @@
+let score = sessionStorage.getItem("score");
+
 window.addEventListener("load", (event) => {
-    // alert(sessionStorage.getItem("score"));
+    alert(score);
+    document.getElementById("scoree").innerText = `${String(sessionStorage.getItem("score"))}%` ;
+    if (sessionStorage.getItem("score") < 26){
+        document.getElementById("mashov").innerText = "דרוש שיפור :("
+    } else {
+        document.getElementById("mashov").innerText = "!כל הכבוד"
+    }
+    document.getElementById("finishBtn").addEventListener("click", finishL);
 });
+
+const finishL = (event) => {
+    window.location.replace("main.html");
+}
+
+// String(sessionStorage.getItem("score"))
